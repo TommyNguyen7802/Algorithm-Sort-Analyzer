ed43733d8dd60e99d880774be58f788ceddda81a
alex-bo7 <anino882@gmail.com>
2025-02-24 19:44:14 -0800

    Implemented Select Display and Better Comments

--- ui.py
+++ ui.py
@@ -1,17 +1,19 @@
 import tkinter as tk
 import random
 import sys
-from AnalyzingAlgorithms import *
 import matplotlib.pyplot as plt
 import numpy as np
+# other python files
+from AnalyzingAlgorithms import *
 
 def generate_random_array(min_value, max_value, num_of_elements):
     # generate random array
     random_array = [random.randint(min_value, max_value) for _ in range(num_of_elements)]
     return random_array
 
-# button functions
+# ---------- button functions ----------
 def show_results_window(): # new window with result/visualization
+    # ----- error checking -----
     # if no algo selected = error
     if wantBubbleSort.get() == 0 and wantMergeSort.get() == 0 and wantQuickSort.get() == 0 \
         and wantRadixSort.get() == 0 and wantLinearSearch.get() == 0:
@@ -20,6 +22,12 @@ def show_results_window(): # new window with result/visualization
     else:
         errors_label.config(text="") # clear error message
 
+     # if no display selected = error
+    if wantDisplayArrays.get() == 0 and wantDisplayGraphs.get() == 0 and wantDisplayAnimations.get() == 0:
+        errors_label.config(text="Error: No display selected", fg="red")
+        return
+    else:
+        errors_label.config(text="") # clear error message
 
     inputs = {
         "minimum_value": min_value_input.get(),
@@ -48,64 +56,71 @@ def show_results_window(): # new window with result/visualization
         errors_label.config(text="") # clear error message
         array = generate_random_array(minimum_value, maximum_value, elements)
 
-    # if no errors then create new window
-    result_window = tk.Toplevel()
-    result_window.title("Result")
+    # create new window that displays arrays
+    if wantDisplayArrays.get() == 1:
+        display_arrays_window = tk.Toplevel()
+        display_arrays_window.title("Result")
 
-    # original array
-    original_arr_label = tk.Label(result_window, text=f"Original Array: {array}", font=body_font)
-    original_arr_label.pack()
+        # original array
+        original_arr_label = tk.Label(display_arrays_window, text=f"Original Array: {array}", font=body_font)
+        original_arr_label.pack()
 
     original_array = array.copy()
     
     algorithms = []
     search_times = {}
 
-    # check selected
+    # ----- check selected -----
     if wantBubbleSort.get() == 1:
         algorithms.append(("Bubble Sort", bubble_sort))
         sorted_array = bubble_sort(array)
-        result_label = tk.Label(result_window, text=f"Sorted Bubble Array: {sorted_array}", font=body_font)
-        result_label.pack()
+        if wantDisplayArrays.get() == 1:
+            result_label = tk.Label(display_arrays_window, text=f"Sorted Bubble Array: {sorted_array}", font=body_font)
+            result_label.pack()
     if wantMergeSort.get() == 1:
         algorithms.append(("Merge Sort", merge_sort))
         sorted_array = merge_sort(array)
-        result_label = tk.Label(result_window, text=f"Sorted Merge Array: {sorted_array}", font=body_font)
-        result_label.pack()
+        if wantDisplayArrays.get() == 1:
+            result_label = tk.Label(display_arrays_window, text=f"Sorted Merge Array: {sorted_array}", font=body_font)
+            result_label.pack()
     if wantQuickSort.get() == 1:
         algorithms.append(("Quick Sort", quick_sort))
         sorted_array = quick_sort(array)
-        result_label = tk.Label(result_window, text=f"Sorted Quick Array: {sorted_array}", font=body_font)
-        result_label.pack()
+        if wantDisplayArrays.get() == 1:
+            result_label = tk.Label(display_arrays_window, text=f"Sorted Quick Array: {sorted_array}", font=body_font)
+            result_label.pack()
     if wantRadixSort.get() == 1:
         algorithms.append(("Radix Sort", radix_sort))
         sorted_array = radix_sort(array)
-        result_label = tk.Label(result_window, text=f"Sorted Radix Array: {sorted_array}", font=body_font)
-        result_label.pack()
+        if wantDisplayArrays.get() == 1:
+            result_label = tk.Label(display_arrays_window, text=f"Sorted Radix Array: {sorted_array}", font=body_font)
+            result_label.pack()
     if wantLinearSearch.get() == 1:
         target_element = int(target_element_input.get())
         target_location = linear_search(original_array, target_element)
-        result_label = tk.Label(result_window, text=f"{target_element} is at index: {target_location}", font=body_font)
-        result_label.pack()
+        if wantDisplayArrays.get() == 1:
+            result_label = tk.Label(display_arrays_window, text=f"{target_element} is at index: {target_location}", font=body_font)
+            result_label.pack()
+        if wantDisplayGraphs.get() == 1:
+            # Measure Search Time
+            search_time = measure_search_time(linear_search, array, target_element)
+            search_times["Linear Search"] = search_time
 
-        # Measure Search Time
-        search_time = measure_search_time(linear_search, array, target_element)
-        search_times["Linear Search"] = search_time
+    if wantDisplayGraphs.get() == 1:
+        # Gather execution times
+        times = {name: measure_sort_time(func, array) for name, func in algorithms}
+        # Combine execution times with search time
+        combined_times = {**times, **search_times}
 
-    # Gather execution times
-    times = {name: measure_sort_time(func, array) for name, func in algorithms}
-    # Combine execution times with search time
-    combined_times = {**times, **search_times}
+        # Create graph
+        plt.figure(figsize=(10, 5))
+        plt.bar(combined_times.keys(), combined_times.values(), color=['blue', 'green'])
+        plt.xlabel('Sorting Algorithm')
+        plt.ylabel('Time (seconds)')
+        plt.title('Execution Time of Sorting Algorithms and Linear Search')
+        plt.show()
 
-    # Create graph
-    plt.figure(figsize=(10, 5))
-    plt.bar(combined_times.keys(), combined_times.values(), color=['blue', 'green'])
-    plt.xlabel('Sorting Algorithm')
-    plt.ylabel('Time (seconds)')
-    plt.title('Execution Time of Sorting Algorithms and Linear Search')
-    plt.show()
-
-# create UI window
+# ---------- main UI window ----------
 root = tk.Tk()
 
 # title, name of application
@@ -115,7 +130,7 @@ root.title("Sorting Alogrithms Visualization")
 header_font = ("Arial", 14, "bold")
 body_font = ("Arial", 12)
 
-## configure input array
+# SECTION::::: configure input array
 configure_input_array_label = tk.Label(root, text="Configure input array settings", font=header_font)
 min_value_label = tk.Label(root, text="Enter minimum value: ", font=body_font)
 max_value_label = tk.Label(root, text="Enter maximum value: ", font=body_font)
@@ -137,8 +152,7 @@ max_value_input.grid(row=2, column=1, sticky="w")
 number_of_elements_label.grid(row=3, column=0, sticky="w")
 number_of_elements_input.grid(row=3, column=1, sticky="w")
 
-
-## select algorithms + UI placement
+# SECTION::::: select algorithms + UI placement
 select_algorithms_label = tk.Label(root, text="Select Algorithms", font=header_font)
 select_algorithms_label.grid(row=4, column=0, columnspan=2, sticky="n")
 
@@ -162,18 +176,30 @@ target_element_input = tk.Spinbox(root, from_=0, to=sys.maxsize, font=body_font,
 target_element_label.grid(row=7, column=1, sticky="w")
 target_element_input.grid(row=7, column=2, sticky="w")
 
-## errors
-errors_label = tk.Label(root, text="", font=body_font)
-errors_label.grid(row=8, columnspan=3, sticky="w")
+# SECTION::::: chose what you show/run
+select_to_display = tk.Label(root, text="Select Display", font=header_font).grid(row=8, columnspan=2, sticky="n")
 
-## buttons
+wantDisplayArrays = tk.IntVar()
+tk.Checkbutton(root, text="Display Arrays", font=body_font, variable=wantDisplayArrays).grid(row=9, column=0, sticky="w")
+
+wantDisplayGraphs = tk.IntVar()
+tk.Checkbutton(root, text="Display Graphs", font=body_font, variable=wantDisplayGraphs).grid(row=9, column=1, sticky="w")
+
+wantDisplayAnimations = tk.IntVar()
+tk.Checkbutton(root, text="Display Animations", font=body_font, variable=wantDisplayAnimations).grid(row=9, column=2, sticky="w")
+
+# SECTION::::: errors
+errors_label = tk.Label(root, text="", font=body_font)
+errors_label.grid(row=10, columnspan=3, sticky="w")
+
+# SECTION::::: buttons
 start_button = tk.Button(root, text="Start", font=body_font, width=20, command=show_results_window)
 quit_button = tk.Button(root, text="Quit", font=body_font, width=20, command=root.destroy)
 
 # buttons UI placement
-start_button.grid(row=9, column=0, sticky="w")
-quit_button.grid(row=9, column=2, sticky="w")
+start_button.grid(row=11, column=0, sticky="w")
+quit_button.grid(row=11, column=2, sticky="w")
 
 
-# start application
+# ---------- start application ----------
 tk.mainloop()
